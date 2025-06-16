eb302397d9342a88ed5ee5d32d8d532e6585e268
Harsh Patil <itsharshhh6@gmail.com>
2025-06-16 09:30:29 +0530

    Using useRef with HTML Elements

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,43 @@ import React, { useRef, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [stateCount, setStateCount] = useState(0);
-  const refCount = useRef(0);
+  // Number One
+  // const [stateCount, setStateCount] = useState(0);
+  // const refCount = useRef(0);
+  // const increment = () => {
+  //   setStateCount(stateCount + 1);
+  // }
+  // const incrementref = () => {
+  //   refCount.current += 1; 
+  //   console.log(`Ref Count: ${refCount.current}`); 
+  // }
 
-
-  const increment = () => {
-    setStateCount(stateCount + 1);
+  const inputRef = useRef(null);
+  const focusInput = () => {
+    inputRef.current.focus();
+    inputRef.current.style.backgroundColor = 'green';
   }
-
-
-  const incrementref = () => {
-    refCount.current += 1; 
-    console.log(`Ref Count: ${refCount.current}`); 
+  const resetFocus = () => {
+    inputRef.current.blur();
+    inputRef.current.style.backgroundColor = 'white';
   }
 
 
   return (
     <div>
-      <h1>State Count : {stateCount}</h1>
+      {/* <h1>State Count : {stateCount}</h1>
       <button onClick={increment}>Increment</button>
-
       <h1>Ref Count : {refCount.current}</h1>
-      <button onClick={incrementref}>Increment</button>
+      <button onClick={incrementref}>Increment</button> */}
+
+      <input type="text" placeholder='Focus me' ref={inputRef}/>
+      <br />
+      <br />
+      <button onClick={focusInput}>Focus</button>
+      <br />
+      <br />
+      <button onClick={resetFocus}>Reset</button>
+
     </div>
   )
 }
