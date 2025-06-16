fc32e72d9e9d7844145429030cbc7cedbb3274fb
Harsh Patil <itsharshhh6@gmail.com>
2025-06-16 09:03:45 +0530

    Basic use of useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-
+import React, { useRef } from 'react'
 import './App.css'
 
 function App() {
- 
+
+  const myRef = useRef("Harsh");
 
   return (
     <div>
-      
+      <h1>Hello, {myRef.current}</h1>
     </div>
   )
 }
