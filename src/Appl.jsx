ce6bc1afed5bc819ee10b1afb8fe9ba4210a61f5
Harsh Patil <itsharshhh6@gmail.com>
2025-06-18 08:12:16 +0530

    Updating a custom hook

--- src/Appl.jsx
+++ src/Appl.jsx
@@ -5,13 +5,14 @@ import useCounter from './useCounter';
 
 
 function Appl() {
-  const {counter, increment, decrement} = useCounter();
+  const {counter, increment, decrement, reset} = useCounter(100);
 
   return (
     <div>
       <h1>Counter: {counter}</h1>
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
+      <button onClick={reset}>Reset</button>
     </div>
   );
 }
--- src/useCounter.jsx
+++ src/useCounter.jsx
@@ -2,7 +2,7 @@ import './App.css';
 import React, { useState } from 'react';
 
 
-function useCounter() {
+function useCounter(initialValue = 0) {
   const [counter, setCounter] = useState(0);
   const increment = () => {
     setCounter(counter + 1);
@@ -10,7 +10,10 @@ function useCounter() {
   const decrement = () => {
     setCounter(counter - 1);
   }
+  const reset = () => {
+    setCounter(initialValue);
+  }
 
-  return {counter, increment, decrement};   
+  return {counter, increment, decrement, reset};   
 }
 export default useCounter;
